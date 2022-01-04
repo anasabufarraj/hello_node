61289b5a4be90f74cfea788ee5526c8f56ab0ba5
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2022-01-04 20:44:41 +0400

    Setup express.js server

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 //------------------------------------------------------------------------------
 // Copyright (c) 2022. Anas Abu Farraj.
 //------------------------------------------------------------------------------
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.write('Hello Homepage!');
-    res.end();
-  }
-
-  if (req.url === '/api') {
-    res.write(JSON.stringify([{ id: 1, name: 'john' }]));
-    res.end();
-  }
+app.get('/', (request, response) => {
+  response.send('Hello Homepage!');
 });
 
-server.on('connection', (socket) => {
-  console.log('Compiled...');
+app.get('/api', (request, response) => {
+  response.send(JSON.stringify([{ id: 1, name: 'john' }]));
 });
 
-server.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
